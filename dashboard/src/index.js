e16f8f9b303a1b6cf34828274cac73d33dae45ba
sakars1 <rakasidebus@gmail.com>
2021-05-31 09:47:08 +0545

    auth not complete

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -17,6 +17,7 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
+import { AuthProvider } from './auth/AuthContext';
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 // core components
