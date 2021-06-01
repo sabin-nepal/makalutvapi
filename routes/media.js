a59cfc4494323d29da1a94cbd9af141672f4bc8f
sabin-nepal <sabinnepal2k17@gmail.com>
2021-06-01 21:23:15 +0545

    media delete

--- routes/media.js
+++ routes/media.js
@@ -1,14 +1,16 @@
 //video routing
 const { Router } = require("express");
 const router = Router();
-
+const { protect } = require('../middelwares/protect.js')
 const{
 	getAll,
 	getMediaById,
+	deletes,
 }	= require('../controllers/media.js');
 
 router.route('/').get(getAll);
 router.route('/:id').get(getMediaById);
+router.route('/delete/:id').post(protect,deletes);
 
 
 module.exports = router; 
