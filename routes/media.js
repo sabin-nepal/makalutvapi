18db38588476812aaf58dccb5c1201f251e7da60
sabin-nepal <sabinnepal2k17@gmail.com>
2021-06-02 14:27:32 +0545

    media upload routes changes

--- routes/media.js
+++ routes/media.js
@@ -2,15 +2,28 @@
 const { Router } = require("express");
 const router = Router();
 const { protect } = require('../middelwares/protect.js')
+const { imageUpload,videoUpload } = require('../helpers/upload.js')
 const{
 	getAll,
 	getMediaById,
 	deletes,
 }	= require('../controllers/media.js');
 
+const{
+	upload,
+	uploads,
+	
+}	= require('../controllers/upload.js');
+
 router.route('/').get(getAll);
 router.route('/:id').get(getMediaById);
 router.route('/delete/:id').post(protect,deletes);
+
+//upload
+
+//router.route('/upload').post(protect,imageUpload.single('thumbnail'),upload);
+router.route('/upload').post(protect,imageUpload.array('thumbnail'),uploads);
+router.route('/upload/video').post(protect,videoUpload.array('video'),upload);
 
 
 module.exports = router; 
