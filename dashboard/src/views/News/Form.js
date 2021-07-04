075a91bfdf0db4343adb3887cf0c6bd545b4b4b0
sakars1 <rakasidebus@gmail.com>
2021-07-04 12:26:36 +0545

    no chnages

--- dashboard/src/views/News/Form.js
+++ dashboard/src/views/News/Form.js
@@ -113,6 +113,8 @@ export default function FormNews(props) {
     });
     setCategory(targetValue);
     setCategoryIdList(catId);
+    console.log(category);
+    console.log(categories);
   };
   const [imageId, setImageId] = React.useState("");
   const [featuredImage, setFeaturedImage] = React.useState("");
